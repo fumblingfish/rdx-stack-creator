af31df700626498e10853a2b5b4c81c2e18d5e19
Rasmus Andersson <holstandersson@gmail.com>
2017-09-16 14:45:02 +0200

    files added

---
+++ index.js
@@ -0,0 +1,113 @@
+import {fromJS} from 'immutable'
+
+
+
+const partialApply = (...leftArgs) =>
+   fn =>
+      (...rightArgs) =>
+         fn.apply(this, [...leftArgs, ...rightArgs])
+
+
+export const PUSH_BY_KEY = 'PUSH_BY_KEY'
+export const pushByKey = (suffix, key, payload) => ({
+   type: `${PUSH_BY_KEY}_${suffix}`,
+   key,
+   payload
+})
+
+
+export const POP = 'POP'
+export const pop = (suffix) => ({
+   type: `${POP}_${suffix}`
+})
+
+export const POP_TO_KEY = 'POP_TO_KEY'
+export const popToKey = (suffix, key) => ({
+   type: `${POP_TO_KEY}_${suffix}`,
+   key
+})
+
+export const POP_TO_INDEX = 'POP_TO_INDEX'
+export const popToIndex = (suffix, index) => ({
+   type: `${POP_TO_INDEX}_${suffix}`,
+   index
+})
+
+
+export const REMOVE_BY_KEY = 'REMOVE_BY_KEY'
+export const removeByKey = (suffix, key) => ({
+   type: `${REMOVE_BY_KEY}_${suffix}`,
+   key
+})
+
+export const REMOVE_ALL = 'REMOVE_ALL'
+export const removeAll = (suffix) => ({
+   type: `${REMOVE_ALL}_${suffix}`,
+})
+
+
+export const initializeStack = (suffix, stackKey) => {
+
+   const partialSuffix = partialApply(suffix)
+
+   return {
+      pushByKey: partialSuffix(pushByKey),
+      pop: partialSuffix(pop),
+      popToKey: partialSuffix(popToKey),
+      popToIndex: partialSuffix(popToIndex),
+      removeByKey: partialSuffix(removeByKey),
+      removeAll: partialSuffix(removeAll),
+      createReducer: createStackReducer(suffix, stackKey)
+   }
+}
+
+export const defaultStackKey = 'stack'
+
+
+const createStackReducer = (suffix = '', stackKey = defaultStackKey) => {
+
+   const itemsEnd = state => state.get(stackKey).size === 0
+
+   return (initialState = {}) => {
+
+      if(!initialState[stackKey]){
+         initialState[stackKey] = []
+      }
+
+      return (state = fromJS(initialState), action) => {
+
+         switch (action.type) {
+            case `${PUSH_BY_KEY}_${suffix}` :
+               const hasItem = state.get(stackKey).find((value) => value.get('key') === action.key)
+               if (hasItem) {
+                  return state
+               }
+               const index = state.get(stackKey).size
+               const item = fromJS({key: action.key, payload: action.payload, index})
+               return state.updateIn([stackKey], list => list.push(item))
+
+            case `${POP}_${suffix}` :
+               return itemsEnd(state) ? state : state.updateIn([stackKey], list => list.pop())
+
+            case `${POP_TO_KEY}_${suffix}`:
+               const lastIndex = state.get(stackKey).findLastIndex((item) => item.get('key') === action.key)
+               if (lastIndex === -1) return state
+               return itemsEnd(state) ? state : state.updateIn([stackKey], list => list.take(lastIndex + 1))
+
+            case `${POP_TO_INDEX}_${suffix}`:
+               return itemsEnd(state) ? state : state.updateIn([stackKey], list => list.take(action.index + 1))
+
+            case `${REMOVE_ALL}_${suffix}`:
+               return state.update(stackKey, list => list.clear())
+
+            case `${REMOVE_BY_KEY}_${suffix}`:
+               return itemsEnd(state)
+                  ? state
+                  : state.updateIn([stackKey], list => list.filter((item) => item.get('key') !== action.key))
+
+            default:
+               return state
+         }
+      }
+   }
+}
---
+++ index.test.js
@@ -0,0 +1,195 @@
+import {initializeStack} from './index';
+import {fromJS} from 'immutable';
+
+const TEST_SUFFIX = 'TEST_SUFFIX'
+const stackKey = 'items'
+
+const {
+   pushByKey,
+   pop,
+   popToKey,
+   popToIndex,
+   removeByKey,
+   removeAll,
+   createReducer
+} = initializeStack('TEST_SUFFIX', stackKey)
+
+
+const reducer = createReducer({})
+
+
+describe('stackReducer', ()=>{
+
+
+   function stateBefore(){
+      return fromJS({
+         items:[{
+            index: 0,
+            key: "A",
+            payload:{title:'Title A'}
+         },{
+            index: 1,
+            key: "B",
+            payload:{title:'Title B'}
+         },{
+            index: 2,
+            key: "C",
+            payload:{title:'Title C'}
+         }]
+      })
+   }
+
+   it('should pushByKey to stack', () => {
+      const action = pushByKey('D', {title:'Title D'})
+      const actualState = reducer(stateBefore(), action).toJS()
+      const expectedState = {
+         items:[{
+            index: 0,
+            key: "A",
+            payload:{title:'Title A'}
+         },{
+            index: 1,
+            key: "B",
+            payload:{title:'Title B'}
+         },{
+            index: 2,
+            key: "C",
+            payload:{title:'Title C'}
+         },{
+            index: 3,
+            key: "D",
+            payload:{title:'Title D'}
+         }]
+      }
+      expect(actualState).toEqual(expectedState)
+   })
+
+
+   it('should pop from stack', () => {
+      const action = pop()
+      const actualState = reducer(stateBefore(), action).toJS()
+      const expectedState = {
+         items:[{
+            index: 0,
+            key: "A",
+            payload:{title:'Title A'}
+         },{
+            index: 1,
+            key: "B",
+            payload:{title:'Title B'}
+         }]
+      }
+      expect(actualState).toEqual(expectedState)
+   })
+
+   it('should pop to neareset key', () => {
+      const action = popToKey('B')
+      const actualState = reducer(stateBefore(), action).toJS()
+      const expectedState = {
+         items:[{
+            index: 0,
+            key: "A",
+            payload:{title:'Title A'}
+         },{
+            index: 1,
+            key: "B",
+            payload:{title:'Title B'}
+         }]
+      }
+      expect(actualState).toEqual(expectedState)
+   })
+
+   it('should return current state on missing key', () => {
+      const action = popToKey('NONE')
+      const actualState = reducer(stateBefore(), action).toJS()
+      const expectedState = {
+         items:[{
+            index: 0,
+            key: "A",
+            payload:{title:'Title A'}
+         },{
+            index: 1,
+            key: "B",
+            payload:{title:'Title B'}
+         },{
+            index: 2,
+            key: "C",
+            payload:{title:'Title C'}
+         }]
+      }
+      expect(actualState).toEqual(expectedState)
+   })
+
+
+   it('should pop to index', () => {
+      const action = popToIndex(1)
+      const actualState = reducer(stateBefore(), action).toJS()
+      const expectedState = {
+         items:[{
+            index: 0,
+            key: "A",
+            payload:{title:'Title A'}
+         },{
+            index: 1,
+            key: "B",
+            payload:{title:'Title B'}
+         }]
+      }
+      expect(actualState).toEqual(expectedState)
+   })
+
+   it('should return current state if index is larger or same as stack size', () => {
+      const action = popToIndex(2)
+      const actualState = reducer(stateBefore(), action).toJS()
+      const expectedState = {
+         items:[{
+            index: 0,
+            key: "A",
+            payload:{title:'Title A'}
+         },{
+            index: 1,
+            key: "B",
+            payload:{title:'Title B'}
+         },{
+            index: 2,
+            key: "C",
+            payload:{title:'Title C'}
+         }]
+      }
+      expect(actualState).toEqual(expectedState)
+      const action2 = popToIndex(3)
+      const actualState2 = reducer(stateBefore(), action2).toJS()
+      expect(actualState2).toEqual(expectedState)
+   })
+
+   it('should remove by key', () => {
+      const action = removeByKey('B')
+      const actualState = reducer(stateBefore(), action).toJS()
+      const expectedState = {
+         items:[{
+            index: 0,
+            key: "A",
+            payload:{title:'Title A'}
+         },{
+            index: 2,
+            key: "C",
+            payload:{title:'Title C'}
+         }]
+      }
+      expect(actualState).toEqual(expectedState)
+   })
+
+   it('should remove all items in stack', () => {
+      const action = removeAll()
+      const actualState = reducer(stateBefore(), action).toJS()
+      const expectedState = {
+         items:[]
+      }
+      expect(actualState).toEqual(expectedState)
+   })
+
+
+
+})
+
+
